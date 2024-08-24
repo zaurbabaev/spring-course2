0ea588724bde2349ff1158677563311bce26d6c5
Zaur <zaurik87@gmail.com>
2024-08-24 18:55:47 +0400

    MVC giriş

---
+++ spring-mvc-main/src/test/java/az/babayev/SpringMvcMainApplicationTests.java
@@ -0,0 +1,13 @@
+package az.babayev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcMainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
